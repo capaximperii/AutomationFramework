cb77c6dd807506e4c619ea7792eb0de35a33b483
capaximperii <luv.singht@gmail.com>
2015-11-23 16:57:23 +0100

    Fixes current rank.
    Fix when the current rank is requested for the depends command.

--- service/ThinClient.py
+++ service/ThinClient.py
@@ -429,9 +429,14 @@ class ThinClient:
     Return the rank of the currently running test case.
     """
     def GetCurrentTestRank(self):
+        cur = 0
         if len(self.testsuite) == len(self.completed):
-            return -1
-        return self.testsuite[len(self.completed) + 1].rank
+            cur = 10000
+        elif len(self.completed) == 0:
+            cur = -10000
+        else :
+            cur = self.testsuite[len(self.completed) - 1].rank
+        return cur
     """
     Output test information for the stats page.
     """
@@ -463,6 +468,7 @@ class ThinClient:
     End the processing for this client and generate report.
     """
     def close(self):
+        print "Closing client"
         testtable = self.recieveTestInfo()
         self.superreport = self.superreport.replace("__TESTSUITE__", testtable)
         self.superreport = self.superreport.replace("__IPADDR__", self.address)
