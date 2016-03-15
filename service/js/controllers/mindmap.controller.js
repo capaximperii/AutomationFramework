dff303a861e5752f1ac3386a06492568191e71f0
luv <luv@henoida.com>
2016-03-15 12:20:59 +0100

    Fixes json crash in mindmap view.

--- service/js/controllers/mindmap.controller.js
+++ service/js/controllers/mindmap.controller.js
@@ -1,7 +1,6 @@
 var app = angular.module('AF');
 
 app.controller('mindCtrl', function ($scope, ip, tests, close) {
-    console.log("controller");
     $scope.ip = ip;
     $scope.close = close;
     $scope.tests = tests;
--- service/js/directives/mindmap.directive.js
+++ service/js/directives/mindmap.directive.js
@@ -2,160 +2,170 @@
 
 var module = angular.module('AF');
 
-module.directive('mindmap', function () {      
-	return {
-		restrict: 'E',
-		template : "<svg id='mindmap'>{{tests}}</svg>",
-		replace: true,
-		scope: {
-			tests: '=tests'
-		},
-		link: function($scope, $element, $attrs) {
+module.directive('mindmap', function() {
+    return {
+        restrict: 'E',
+        template: "<svg id='mindmap'>{{tests}}</svg>",
+        replace: true,
+        scope: {
+            tests: '=testdata'
+        },
+        link: function($scope, $element, $attrs) {
 
-			$scope.$watch($scope.tests, function(oldval, newval){
-				if ($scope.tests == null ) return;
-				draw($scope.tests);
-			});
+            $scope.$watch($scope.tests, function(oldval, newval) {
+                if ($scope.tests == null) return;
+                // unlink 2 way binding for this data here so that d3 can modify it.
+                var copy = JSON.stringify($scope.tests);
+                draw(JSON.parse(copy));
+            });
 
-			function draw(tests) {
+            function draw(root) {
 
-				var margin = {top: 20, right: 120, bottom: 20, left: 120},
-				width = 960 - margin.right - margin.left,
-				height = 800 - margin.top - margin.bottom;
+                var margin = { top: 20, right: 120, bottom: 20, left: 120 },
+                    width = 960 - margin.right - margin.left,
+                    height = 800 - margin.top - margin.bottom;
+                var i = 0,
+                    duration = 750;
 
-				var i = 0,
-				duration = 750,
-				root;
+                var tree = d3.layout.tree()
+                    .size([height, width]);
 
-				var tree = d3.layout.tree()
-				.size([height, width]);
+                var diagonal = d3.svg.diagonal()
+                    .projection(function(d) {
+                        return [d.y, d.x]; });
 
-				var diagonal = d3.svg.diagonal()
-				.projection(function(d) { return [d.y, d.x]; });
-				
-				var svg = $element
-				.attr("width", width + margin.right + margin.left)
-				.attr("height", height + margin.top + margin.bottom)
-				.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-				root = tests;
+                var svg = $element
+                    .attr("width", width + margin.right + margin.left)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-				root.x0 = height / 2;
-				root.y0 = 0;
+                root.x0 = height / 2;
+                root.y0 = 0;
 
-				function collapse(d) {
-					if (d.children) {
-						d._children = d.children;
-						d._children.forEach(collapse);
-						d.children = null;
-					}
-				}
+                function collapse(d) {
+                    if (d.children) {
+                        d._children = d.children;
+                        d._children.forEach(collapse);
+                        d.children = null;
+                    }
+                }
 
-				root.children.forEach(collapse);
-				update(root);
+                root.children.forEach(collapse);
+                update(root);
 
-				d3.select(self.frameElement).style("height", "800px");
+                d3.select(self.frameElement).style("height", "800px");
 
-				function update(source) {
-			  // Compute the new tree layout.
-			  var nodes = tree.nodes(root).reverse(),
-			  links = tree.links(nodes);
+                function update(source) {
+                    // Compute the new tree layout.
+                    var nodes = tree.nodes(root).reverse(),
+                        links = tree.links(nodes);
 
-			  // Normalize for fixed-depth.
-			  nodes.forEach(function(d) { d.y = d.depth * 180; });
+                    //Normalize for fixed-depth.
+                    nodes.forEach(function(d) { d.y = d.depth * 180; });
 
-			  // Update the nodes…
-			  var node = d3.select("#mindmap").selectAll("g.node")
-			  .data(nodes, function(d) { return d.id || (d.id = ++i); });
+                    // Update the nodes…
+                    var node = d3.select("#mindmap").selectAll("g.node")
+                        .data(nodes, function(d) {
+                            return d.id || (d.id = ++i); });
 
-			  // Enter any new nodes at the parent's previous position.
-			  var nodeEnter = node.enter().append("g")
-			  .attr("class", "node")
-			  .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-			  .on("click", click);
+                    // Enter any new nodes at the parent's previous position.
+                    var nodeEnter = node.enter().append("g")
+                        .attr("class", "node")
+                        .attr("transform", function(d) {
+                            return "translate(" + source.y0 + "," + source.x0 + ")"; })
+                        .on("click", click);
 
-			  nodeEnter.append("circle")
-			  .attr("r", 1e-6)
-			  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+                    nodeEnter.append("circle")
+                        .attr("r", 1e-6)
+                        .style("fill", function(d) {
+                            return d._children ? "lightsteelblue" : "#fff"; });
 
-			  nodeEnter.append("text")
-			  .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-			  .attr("dy", ".35em")
-			  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-			  .text(function(d) { return d.name; })
-			  .style("fill-opacity", 1e-6);
+                    nodeEnter.append("text")
+                        .attr("x", function(d) {
+                            return d.children || d._children ? -10 : 10; })
+                        .attr("dy", ".35em")
+                        .attr("text-anchor", function(d) {
+                            return d.children || d._children ? "end" : "start"; })
+                        .text(function(d) {
+                            return d.name; })
+                        .style("fill-opacity", 1e-6);
 
-			  // Transition nodes to their new position.
-			  var nodeUpdate = node.transition()
-			  .duration(duration)
-			  .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+                    // Transition nodes to their new position.
+                    var nodeUpdate = node.transition()
+                        .duration(duration)
+                        .attr("transform", function(d) {
+                            return "translate(" + d.y + "," + d.x + ")"; });
 
-			  nodeUpdate.select("circle")
-			  .attr("r", 4.5)
-			  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+                    nodeUpdate.select("circle")
+                        .attr("r", 4.5)
+                        .style("fill", function(d) {
+                            return d._children ? "lightsteelblue" : "#fff"; });
 
-			  nodeUpdate.select("text")
-			  .style("fill-opacity", 1);
+                    nodeUpdate.select("text")
+                        .style("fill-opacity", 1);
 
-			  // Transition exiting nodes to the parent's new position.
-			  var nodeExit = node.exit().transition()
-			  .duration(duration)
-			  .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-			  .remove();
+                    // Transition exiting nodes to the parent's new position.
+                    var nodeExit = node.exit().transition()
+                        .duration(duration)
+                        .attr("transform", function(d) {
+                            return "translate(" + source.y + "," + source.x + ")"; })
+                        .remove();
 
-			  nodeExit.select("circle")
-			  .attr("r", 1e-6);
+                    nodeExit.select("circle")
+                        .attr("r", 1e-6);
 
-			  nodeExit.select("text")
-			  .style("fill-opacity", 1e-6);
+                    nodeExit.select("text")
+                        .style("fill-opacity", 1e-6);
 
-			  // Update the links…
-			  var link = d3.select("#mindmap").selectAll("path.link")
-			  .data(links, function(d) { return d.target.id; });
+                    // Update the links…
+                    var link = d3.select("#mindmap").selectAll("path.link")
+                        .data(links, function(d) {
+                            return d.target.id; });
 
-			  // Enter any new links at the parent's previous position.
-			  link.enter().insert("path", "g")
-			  .attr("class", "link")
-			  .attr("d", function(d) {
-			  	var o = {x: source.x0, y: source.y0};
-			  	return diagonal({source: o, target: o});
-			  });
+                    // Enter any new links at the parent's previous position.
+                    link.enter().insert("path", "g")
+                        .attr("class", "link")
+                        .attr("d", function(d) {
+                            var o = { x: source.x0, y: source.y0 };
+                            return diagonal({ source: o, target: o });
+                        });
 
-			  // Transition links to their new position.
-			  link.transition()
-			  .duration(duration)
-			  .attr("d", diagonal);
+                    // Transition links to their new position.
+                    link.transition()
+                        .duration(duration)
+                        .attr("d", diagonal);
 
-			  // Transition exiting nodes to the parent's new position.
-			  link.exit().transition()
-			  .duration(duration)
-			  .attr("d", function(d) {
-			  	var o = {x: source.x, y: source.y};
-			  	return diagonal({source: o, target: o});
-			  })
-			  .remove();
+                    // Transition exiting nodes to the parent's new position.
+                    link.exit().transition()
+                        .duration(duration)
+                        .attr("d", function(d) {
+                            var o = { x: source.x, y: source.y };
+                            return diagonal({ source: o, target: o });
+                        })
+                        .remove();
 
-			  // Stash the old positions for transition.
-			  nodes.forEach(function(d) {
-			  	d.x0 = d.x;
-			  	d.y0 = d.y;
-			  });
-			}
+                    // Stash the old positions for transition.
+                    nodes.forEach(function(d) {
+                        d.x0 = d.x;
+                        d.y0 = d.y;
+                    });
+                }
 
-			// Toggle children on click.
-			function click(d) {
-				if (d.children) {
-					d._children = d.children;
-					d.children = null;
-				} else {
-					d.children = d._children;
-					d._children = null;
-				}
-				update(d);
-			}	
-		}
+                // Toggle children on click.
+                function click(d) {
+                    if (d.children) {
+                        d._children = d.children;
+                        d.children = null;
+                    } else {
+                        d.children = d._children;
+                        d._children = null;
+                    }
+                    update(d);
+                }
+            }
 
-	}
-}	
+        }
+    }
 
 });
