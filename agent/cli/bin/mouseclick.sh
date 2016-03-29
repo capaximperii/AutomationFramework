0cd5676db1b98ce0ee9200024fb1b3aa89203958
luv <capaximperii@github.com>
2016-03-29 14:22:46 +0200

    Allows clicking desktop icons

--- agent/cli/bin/mouseclick.sh
+++ agent/cli/bin/mouseclick.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#usage mouseclick.sh <wid> <screenshot> PATTERN_FILES...
+# usage mouseclick.sh <wid> <screenshot> PATTERN_FILES...
+# use wid 0 for desktop icons
 # used commands
 xte_command=$(which xte)
 visgrep_command=$(which visgrep)
@@ -17,8 +18,21 @@ if [ -n "$xte_command" -a -n "$xwininfo_command" ]; then
 	for pattern in $@
 		do
 		rpos=$($visgrep_command $windowimg $pattern)
-		echo $rpos
-		if [ -n "$rpos" ]; then
+		if [ "$wid" -eq "0" ]; then		
+			# get pattern position (whole desktop)
+			x2=$(echo $rpos | sed "s/\(.*\),.*/\1/")
+			y2=$(echo $rpos | sed "s/.*,\(.*\) -1/\1/")
+
+			# calculate resulting position (add '10' to be inside)
+			x=$(expr $x2 + 10)
+			y=$(expr $y2 + 10)
+	
+			echo Found pattern at screen location x=$x, y$y
+			# minimize all windows			
+			# wmctrl -k off
+			$xte_command "mousemove $x $y" "sleep 1" "mouseclick 1" "sleep 1"
+			mousemove=0
+		elif [ -n "$rpos" ]; then
 			# get window position
 			x1=$($xwininfo_command -id $wid | awk '/Absolute upper-left X/ {print $NF}')
 			y1=$($xwininfo_command -id $wid | awk '/Absolute upper-left Y/ {print $NF}')
