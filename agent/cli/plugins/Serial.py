7aa8efcba72e33d41a9bfd8206469c45947eda6f
Luv <luv.singh@crevise.com>
2017-12-05 23:11:14 +0530

    Implements serial command

---
+++ agent/cli/plugins/Serial.py
@@ -0,0 +1,79 @@
+from cli.core.Console import Console
+from cli.core.docopt import docopt
+from cli.core.ExecHelper import ExecHelper
+import sys
+
+from serial import Serial
+from serial import STOPBITS_TWO, EIGHTBITS, PARITY_NONE
+from xmodem import XMODEM
+
+class Cli(Console):
+    def __init__(self):
+        Console.__init__(self)
+        self.doc = """
+        Usage:
+            Serial command <device> <cmdline> <wait>
+            Serial sendfile <device> <path> <wait>
+            Serial (-h | --help | --version)
+
+        Options:
+            -h, --help  Show this screen.
+        """
+    def getc(self, size, timeout=1):
+        return self.ser.read(size) or None
+
+    def putc(self, data, timeout=1):
+        return self.ser.write(data)
+
+    def do_Serial(self, args):
+        """
+        """
+        command = docopt(str(self.doc), args)
+        e = ExecHelper()
+        e.reset("Serial")
+        if command['command']:
+            with Serial(port=command['<device>'], baudrate=115200,
+                        stopbits=STOPBITS_TWO, bytesize=EIGHTBITS,
+                        parity=PARITY_NONE, dsrdtr=True, rtscts=True) as ser:
+                for c in command['<cmdline>']:
+                    ser.write(c)
+                while True:
+                    buf = ser.read()
+                    if command['<wait>'] in buf:
+                        break
+            pass
+        elif command['sendfile']:
+            with Serial(port=command['<device>'], baudrate=115200,
+                        stopbits=STOPBITS_TWO, bytesize=EIGHTBITS,
+                        parity=PARITY_NONE, dsrdtr=True, rtscts=True) as self.ser:
+                modem = XMODEM(self.getc, self.putc)
+                stream = open(command['<path>'], 'rb')
+                modem.send(stream)
+                while True:
+                    buf = ser.read()
+                    if command['<wait>'] in buf:
+                        break
+
+        e.Log(e.r.getOutput())
+
+    def do_help(self, arg):
+        """
+        """
+        print(self.doc)
+
+def setup():
+    """
+    """
+    prompt = Cli()
+    p = __name__.split('.')
+    prompt.prompt = p[len(p) - 1] + " > "
+    return prompt
+
+def main():
+    """
+    """
+    prompt = setup()
+    prompt.cmdloop('Starting cli...')
+
+if __name__ == '__main__':
+    main()
